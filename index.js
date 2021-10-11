923ea89ce9448c85a3b0de0a9090ee043da53e8a
thien <lehollo09112000@gmail.com>
2021-10-11 23:17:28 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+$('.customer__list').slick({
+    infinite: false,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    dots:true,
+    nextArrow:`<i class="fas fa-arrow-right nextArrow"></i>`,
+    prevArrow:`<i class="fas fa-arrow-left prevArrow"></i>`,
+  });
